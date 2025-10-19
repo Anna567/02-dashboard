d3d143a3c27f630fa2ec70de9ea8607cbf4b6b83
Anna Asc <39515604+Anna567@users.noreply.github.com>
2025-10-19 14:19:25 +0200

    Fix type declaration for listItem in Menu component

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,13 +2,22 @@ import { Link } from "react-router-dom";
 import "./menu.scss";
 import { menu } from "../../data";
 
+type MenuLink = {
+  id: number;
+  title: string;
+  url: string;
+  icon: string;
+};
+
+
 const Menu = () => {
   return (
     <div className="menu">
       {menu.map((item) => (
         <div className="item" key={item.id}>
           <span className="title">{item.title}</span>
-          {item.listItems.map((listItem) => (
+          //any- misuse
+          {item.listItems.map((listItem:any) => (
             <Link to={listItem.url} className="listItem" key={listItem.id}>
               <img src={listItem.icon} alt="" />
               <span className="listItemTitle">{listItem.title}</span>
